a3e7c51d90d8c40d9b69772f5b8aec23601ad8f3
Sunnybest008 <bestsunny6@gmai.com>
2023-10-11 07:47:11 -0700

    0x0D-preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,12 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 #include <stdio.h>
 
 int main(void)
 {
-    printf("%s\n", __FILE__);
-    return 0;
+	printf("%s\n", __FILE__);
+	return (0);
 }
